5dec25a45b1dc19fa9c378a7c012aa4a3eb937b2
Rongbing Lai <rongbil1@uci.edu>
2024-04-12 09:37:42 -0700

    get path by title tool and fetch section content by titles tool

--- tools/tools.py
+++ tools/tools.py
@@ -1,7 +1,7 @@
 import re
 import os
 from pdfminer.high_level import extract_pages
-from pdfminer.layout import LTTextContainer, LTChar
+from pdfminer.layout import LTTextContainer
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
@@ -11,9 +11,7 @@ from langchain.tools import tool
 
 @tool
 def fetch_all_section_titles(path: str) -> list:
-    """
-    Fetches all section titles in a specified research paper.
-    
+    """    
     This function searches for the given path, extracts pages in the pdf file
     parses its content, and extracts the titles of all sections. This can include 
     standard sections such as 'Introduction', 'Literature Review', 'Methodology', etc.
@@ -44,6 +42,62 @@ def fetch_all_section_titles(path: str) -> list:
     return section_titles
 
 @tool
+def get_path_by_title(title: str) -> str:
+    """
+    Given the title, this function returns the local path of the research paper.
+
+    Parameters: 
+        title (str): The title of the research paper.
+    
+    Returns:
+        str: The path of the research paper location on the local computer.
+    """
+    return "/Users/crystalalice/Desktop/ICSHP_Research/SE_paper/" + title + ".pdf"
+
+@tool
+def fetch_section_content_by_titles(section_title: str, path: str) -> str:
+    """
+    The function fetechs the content of a research paper section for the given section
+    title from the given local path. This include standard section titles such as 
+    Introduction, Methodology and etc.
+
+    Parameters: 
+        section_title (str): The title of the research paper section.
+        path (str): The path of the research paper in the local computer.
+    
+    Returns: 
+        str: The content of the research paper section.
+
+    Examples:
+        If the given section title is I.Introduction, then the function will return the content
+        of the Introduction section from the given path:
+        "Good old documentation, the ideal companion of any software system, is intended to provide 
+        stakeholders with useful knowledge about the system and related processes...address them."
+    """
+    title_pattern = re.compile(r'^' + re.escape(section_title) + r'$', re.IGNORECASE)
+    next_section_pattern = re.compile(r'^[IVXLCDM]+\.\s|\bREFERENCES\b|\ACKNOWLEDGMENT\b', re.IGNORECASE)
+    
+    content_started = False
+    content = ""
+
+    for page_layout in extract_pages(path):
+        for element in page_layout:
+            if isinstance(element, LTTextContainer):
+                for text_line in element:
+                    line_text = text_line.get_text().strip()
+                    # records the start of a section
+                    if title_pattern.match(line_text):
+                        content_started = True
+                        continue
+                    # returns the content if reaches the next section title
+                    if content_started and next_section_pattern.match(line_text):
+                        return content
+                    
+                    if content_started:
+                        content += line_text    
+    return content
+
+@tool
 def get_relevant_section(question: str):
     # make it longer, explain a tool to someone, give examples
     """
@@ -61,19 +115,3 @@ def get_relevant_section(question: str):
 
     res = qa.invoke(question)
     return res
-
-
-# @tool
-# def get_relevant_section():
-#     """
-#     Retrieve the most relevant section from the indexed
-#     papers that evaluates research paper based on a given question.
-#     """
-#     embeddings = OpenAIEmbeddings()
-#     vectorstore = FAISS.load_local("faiss_index_criteria", embeddings, allow_dangerous_deserialization=True)
-#     retriever = vectorstore.as_retriever()
-
-#     return create_retriever_tool(retriever,
-#                                  "get_relevant_section",
-#                                  "Retrieve the most relevant section from the indexed \
-#                                   papers that evaluates research paper based on a given question.")
