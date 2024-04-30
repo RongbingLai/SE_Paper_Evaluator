fe730bd973fc5978d1aa3e6bb64f8ffda7700289
Rongbing Lai <rongbil1@uci.edu>
2024-04-29 22:03:00 -0700

    add fetch_all_section_titles, fetch_section_content_by_titles, generate_review tools

--- tools/tools.py
+++ tools/tools.py
@@ -1,5 +1,3 @@
-import re
-import os
 from pdfminer.high_level import extract_pages
 from pdfminer.layout import LTTextContainer
 from langchain_openai import OpenAIEmbeddings
@@ -7,78 +5,60 @@ from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 from langchain_openai import OpenAI
 from langchain.tools import tool
+from langchain_core.prompts import PromptTemplate
+from papermage.recipes import CoreRecipe
+import json
 
+section_title_list = []
 
 @tool
 def fetch_all_section_titles(path: str) -> list:
-    """    
-    This function searches for the given path, extracts pages in the pdf file
-    parses its content, and extracts the titles of all sections. This can include 
-    standard sections such as 'Introduction', 'Literature Review', 'Methodology', etc.
-    
+    """
+    This function searches for the given path and extracts the titles of all sections. 
+
     Parameters:
         path (str): The path of the research paper from which to fetch the section titles.
-        
+
     Returns:
         list: A list of section titles found in the specified research paper.
-    
+
     Example:
         If the research paper has sections 'Introduction', 'Literature Review', 'Methodology',
         the function will return:
         ['Introduction', 'Literature Review', 'Methodology']
     """
-    # This is only suitable for the current used paper
-    title_pattern = re.compile(r'^[IVXLCDM]+\.\s+[A-Z\s]+$', re.MULTILINE)
-    section_titles = []
+    recipe = CoreRecipe()
+    doc = recipe.run(path)
+    for section in doc.sections:
+        section_title_list.append(section.text)
 
-    for page_layout in extract_pages(path):
-        for element in page_layout:
-            if isinstance(element, LTTextContainer):
-                for text_line in element:
-                    line_text = text_line.get_text().strip()
-                    if title_pattern.match(line_text):
-                        section_titles.append(line_text)
-
-    return section_titles
+    return section_title_list
 
 @tool
-def get_path_by_title(title: str) -> str:
-    """
-    Given the title, this function returns the local path of the research paper.
-
-    Parameters: 
-        title (str): The title of the research paper.
-    
-    Returns:
-        str: The path of the research paper location on the local computer.
-    """
-    return "/Users/crystalalice/Desktop/ICSHP_Research/SE_paper/" + title + ".pdf"
-
-@tool
-def fetch_section_content_by_titles(section_title: str, path: str) -> str:
+def fetch_section_content_by_titles(params) -> str:
     """
     The function fetechs the content of a research paper section for the given section
-    title from the given local path. This include standard section titles such as 
-    Introduction, Methodology and etc.
+    title from the given local path. 
 
-    Parameters: 
-        section_title (str): The title of the research paper section.
-        path (str): The path of the research paper in the local computer.
-    
-    Returns: 
+    Parameters:
+        params (str): a string dictionary that contains the actual parameters. The first key is "section_title" with 
+        the title of the research paper section as the value. The second key is "path" with the path of the 
+        research paper in the local computer as the value
+
+    Returns:
         str: The content of the research paper section.
 
     Examples:
         If the given section title is I.Introduction, then the function will return the content
         of the Introduction section from the given path:
-        "Good old documentation, the ideal companion of any software system, is intended to provide 
+        "Good old documentation, the ideal companion of any software system, is intended to provide
         stakeholders with useful knowledge about the system and related processes...address them."
     """
-    title_pattern = re.compile(r'^' + re.escape(section_title) + r'$', re.IGNORECASE)
-    next_section_pattern = re.compile(r'^[IVXLCDM]+\.\s|\bREFERENCES\b|\ACKNOWLEDGMENT\b', re.IGNORECASE)
-    
     content_started = False
     content = ""
+    next_index = 0
+    section_title = json.loads(params)['section_title']
+    path = json.loads(params)['path']
 
     for page_layout in extract_pages(path):
         for element in page_layout:
@@ -86,32 +66,85 @@ def fetch_section_content_by_titles(section_title: str, path: str) -> str:
                 for text_line in element:
                     line_text = text_line.get_text().strip()
                     # records the start of a section
-                    if title_pattern.match(line_text):
+                    if line_text.replace(" ", "") == section_title.replace(" ", ""):
                         content_started = True
+                        next_index = section_title_list.index(section_title) + 1
                         continue
+
                     # returns the content if reaches the next section title
-                    if content_started and next_section_pattern.match(line_text):
+                    if content_started and (line_text.replace(" ", "") == section_title_list[next_index].replace(" ","")):
                         return content
-                    
+
                     if content_started:
-                        content += line_text    
+                        content += line_text + "\n"
     return content
 
 @tool
-def get_relevant_section(question: str):
-    # make it longer, explain a tool to someone, give examples
+def generate_review(section_content: str, question: str) -> str:
     """
-    Retrieve the most relevant section from the indexed
-    papers that evaluates research paper based on a given question.
+    The function returns a response that answers the question regarding the section
+    content by searching in the local faiss database.
+
+    Parameters:
+        question (str): The question given in the prompt context.
+        section_content (str): The section content from the manuscript.
+
+    Returns:
+        str: The response answering the given question that evaluates the section_content.
     """
     embeddings = OpenAIEmbeddings()
     vectorstore = FAISS.load_local(
-        "faiss_index_criteria", embeddings, allow_dangerous_deserialization=True
+        "/Users/crystalalice/Desktop/ICSHP_Research/SE_Paper_Evaluator/faiss_index_criteria", embeddings, allow_dangerous_deserialization=True
     )
     retriever = vectorstore.as_retriever()
+    llm = OpenAI()
     qa = RetrievalQA.from_chain_type(
-        llm=OpenAI(), chain_type="stuff", retriever=retriever
+        llm=llm, chain_type="stuff", retriever=retriever
     )
 
-    res = qa.invoke(question)
+    try:
+        definition = qa.invoke("What kind of result is considered to be specific and concrete? What kind of results are not precise?")
+        print(definition)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    temp = '''
+    Given the section below, answer the question in detail with reference sentences from the section content.
+
+    Section Content: 
+    {section_content}
+    END OF SECTION
+
+    Assessment Criteria:
+    {criterion_definition}
+
+    Question:
+    {question}
+
+    In your final answer, you need to include two parts using this format:
+        ```
+        Manuscript Text: [the sentence(s) that you want to review in the section content]
+        Review: [Your evaluation]
+        ... (you should have enough Manuscript Text/Review to cover the review for the whole section content)
+        ```
+    '''
+
+    # question = (
+    #     f"Given the section below, answer the question.\n"
+    #     "Section Content:\n{section_content}\nEND OF SECTION\n\n"
+    #     "Assessment Criteria:\n{criterion_definition}"
+    #     "Question: {question}"
+    # )
+
+    prompt = PromptTemplate(template=temp, input_variables=["section_content","criterion_definition","question"])
+    chain = prompt | llm
+    res = chain.invoke({
+        "section_content": section_content,
+        "criterion_definition": definition,
+        "question": question,
+    })
     return res
+
+# @tool
+# def get_review_from_similar_paper():
+#     pass
