e85170de00b2f3dbb7ffa60aba5959a49986dee6
Rongbing Lai <rongbil1@uci.edu>
2024-04-29 21:59:30 -0700

    move paper path variable to the agent file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from agents.paper_evaluate_agent import evaluate_paper
-
-PAPER_PATH = "/Users/crystalalice/Desktop/ICSHP_Research/SE_paper/Software_Documentation_Issues_Unveiled.pdf"
+from dotenv import load_dotenv
 
 if __name__ == "__main__":
-    evaluate_paper(PAPER_PATH)
+    load_dotenv()
+    evaluate_paper()
