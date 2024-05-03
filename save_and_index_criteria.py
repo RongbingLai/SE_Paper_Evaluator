85e96fe11f8e256b500034f55565bc318d705a09
Rongbing Lai <rongbil1@uci.edu>
2024-05-02 22:43:59 -0700

    change to sementic chunker

--- save_and_index_criteria.py
+++ save_and_index_criteria.py
@@ -1,11 +1,13 @@
 import os
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
+import time
+from langchain_experimental.text_splitter import SemanticChunker
+from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFLoader
 
-CRITERIA_PAPERS_DIRECTORY = "/Users/crystalalice/Desktop/ICSHP_Research/criteria_papers_for_database"
-
+CRITERIA_PAPERS_DIRECTORY = (
+    "/Users/crystalalice/Desktop/ICSHP_Research/criteria_papers_for_database"
+)
 
 def save_and_index_papers():
     embeddings = OpenAIEmbeddings()
@@ -16,12 +18,14 @@ def save_and_index_papers():
             pdf_path = os.path.join(CRITERIA_PAPERS_DIRECTORY, filename)
             loader = PyPDFLoader(file_path=pdf_path)
             documents = loader.load()
-            text_splitter = CharacterTextSplitter(
-                chunk_size=1000, chunk_overlap=30, separator="\n"
+            text_splitter = SemanticChunker(
+                OpenAIEmbeddings(show_progress_bar=True)
             )
             docs = text_splitter.split_documents(documents=documents)
-            all_docs.extend(docs)
-            print("splitted")
+            for doc in docs:
+                all_docs.append(doc)
+                print("added doc")
+                time.sleep(2)
 
     vectorstore = FAISS.from_documents(all_docs, embeddings)
     vectorstore.save_local("faiss_index_criteria")
