f87cae560750913106f1ad5962c3a372c1af2b65
Rongbing Lai <rongbil1@uci.edu>
2024-05-29 15:32:14 -0700

    revise save and index criteria faiss vectorstore

--- save_and_index_criteria.py
+++ save_and_index_criteria.py
@@ -5,9 +5,8 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFLoader
 
-CRITERIA_PAPERS_DIRECTORY = (
-    "/Users/crystalalice/Desktop/ICSHP_Research/criteria_papers_for_database"
-)
+CRITERIA_PAPERS_DIRECTORY = "/Users/crystalalice/Desktop/ICSHP_Research/criteria papers"
+
 
 def save_and_index_papers():
     embeddings = OpenAIEmbeddings()
@@ -18,9 +17,7 @@ def save_and_index_papers():
             pdf_path = os.path.join(CRITERIA_PAPERS_DIRECTORY, filename)
             loader = PyPDFLoader(file_path=pdf_path)
             documents = loader.load()
-            text_splitter = SemanticChunker(
-                OpenAIEmbeddings(show_progress_bar=True)
-            )
+            text_splitter = SemanticChunker(OpenAIEmbeddings(show_progress_bar=True))
             docs = text_splitter.split_documents(documents=documents)
             for doc in docs:
                 all_docs.append(doc)
@@ -28,7 +25,7 @@ def save_and_index_papers():
                 time.sleep(2)
 
     vectorstore = FAISS.from_documents(all_docs, embeddings)
-    vectorstore.save_local("faiss_index_criteria")
+    vectorstore.save_local("faiss_index_full_criteria")
 
     print("Indexed and saved criteria papers.")
 
