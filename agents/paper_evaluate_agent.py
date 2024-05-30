5f3cdbbf54973728cf4ca79f32434614947f95bd
Rongbing Lai <rongbil1@uci.edu>
2024-05-30 16:11:13 -0700

    revised prompt template

--- agents/paper_evaluate_agent.py
+++ agents/paper_evaluate_agent.py
@@ -14,31 +14,64 @@ os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 
 def evaluate_paper():
+#     old_template = """
+# You are a Software Engineering Research Paper committee reviewer from a top 
+# conference. Your task is to evaluate the quality of a submission stated in the user 
+# input.
+
+# You will be given by a path of the manuscript that needs to be reviewed.
+# You are supposed to review the manuscript section by section. You need to complete review all sections
+# before you stop. 
+
+# You should use a tool to generate the review of a section in the manuscript. 
+
+# You need to remember all section titles so that you are able to review the sections.
+# In each section, for every sentence that you think needs to improve, you will need to
+# take notes of that sentence. It will be used in your final answer.
+
+# You have access to the following tools:
+# {tools}
+
+# Follow the exact step-by-step answering process in reviewing the manuscript:
+
+# ```
+# Thought: You should always think what you want to do
+# Action: the action to take, should be exactly one of [{tool_names}]
+# Action Input: the input to the action
+# Observation: the result of the action that will be returned by the tool (DO NOT GENERATE THIS PART)
+# ... (this Thought/Action/Action Input/Observation can repeat N times)
+# ```
+
+# When you have a response to say to the Human, or if you do not need to use a tool, you MUST use the format:
+
+# ```
+# Thought: Do I need to use a tool? No
+# Final Answer: [your response here]
+# ```
+
+# Begin!
+
+# Question: {input}
+# {agent_scratchpad}
+#     """
+
     template = """
 You are a Software Engineering Research Paper committee reviewer from a top 
 conference. Your task is to evaluate the quality of a submission stated in the user 
-input.
+input. You only need to review the section. 
 
-You will be given by a path of the manuscript that needs to be reviewed.
-You are supposed to review the manuscript section by section. You need to complete review all sections
-before you finish. 
+You will be given by a path of the manuscript that needs to be reviewed, and a title of the section
+that you needs to review.
 
-You should use a tool to fetch all the section titles from the manuscript.
 You should use a tool to generate the review of a section in the manuscript. 
-You should use a tool to get the actual reviews for a research paper that is similar to the manuscript.
 
-Your action order should be:
-1. Get manuscript section titles
-2. [Optional] Check if there are reviews for similiar research paper and get reviews as reference
-3. For each section, generate review for the section by asking evaluation questions from the list
-4. Repeat step 2-3 until you reviewed all of the sections listed
-
-You need to remember all the section titles so that you are able to review the sections.
-In each section, for every sentence that you think needs to improve, you will need to
-take notes of that sentence. It will be used in your final answer.
+You need to review the section sentence by sentence. For every sentence that you would like to leave
+comment on, you need to take notes of that sentence. It will be used in your final answer.
 
 You have access to the following tools:
 {tools}
+
+You must always use one of the tools as your action.
 
 Follow the exact step-by-step answering process in reviewing the manuscript:
 
@@ -46,7 +79,7 @@ Follow the exact step-by-step answering process in reviewing the manuscript:
 Thought: You should always think what you want to do
 Action: the action to take, should be exactly one of [{tool_names}]
 Action Input: the input to the action
-Observation: the result of the action that will be returned by the tool (DO NOT GENERATE THIS PART)
+Observation: the result of the action that will be returned by the tool
 ... (this Thought/Action/Action Input/Observation can repeat N times)
 ```
 
@@ -57,6 +90,17 @@ Thought: Do I need to use a tool? No
 Final Answer: [your response here]
 ```
 
+Your final review result must be a JSON blob structured as below:
+```
+{{
+"Manuscript Text": $manuscript text,
+"Comment": $comment
+}}
+```
+
+- $Manuscript Text should be a sentence or sentences that you want to leave comment on. Manuscript Text must be from the section content of the manuscript
+- $Comment should be a constructive and practical review for the manuscript's author
+
 Begin!
 
 Question: {input}
@@ -65,19 +109,6 @@ Question: {input}
 
     # list of tools
     tools = [
-        # Tool(
-        #     name="Fetch All Section Titles",
-        #     func=fetch_all_section_titles,
-        #     description="Use this tool when you want to fetch all section titles from the manuscript. \
-        #                  Input should be a string of path to the manuscript.",
-        # ),
-        # Tool(
-        #     name="Fetch Section Content by Title",
-        #     func=fetch_section_content_by_titles,
-        #     description="Use this tool when you want to fetch the content of a section from the manuscript. \
-        #                  Use this tool with arguments like `{``section_title``: str, ``path``: str}` when you need to \
-        #                  retrieve the section content. Use double quotes for key and value strings in the format.",
-        # ),
         Tool(
             name="Generate Review",
             func=generate_review,
@@ -95,10 +126,9 @@ Question: {input}
     section_title_list = fetch_all_section_titles()
     section_title_str = ("\n").join(section_title_list)
 
-    initial_input = (
-        f"Can you please review the the manuscript located at: '{path}'?\n\nHere are the section titles of the manuscript:\n{section_title_str}"
-    )
-    print(initial_input)
+    # initial_input = (
+    #     f"Can you please review the the manuscript located at: '{path}'?\n\nHere are the section titles of the manuscript:\n{section_title_str}"
+    # )
 
     llm = OpenAI(temperature=0, model="gpt-3.5-turbo-instruct")
     
@@ -115,8 +145,16 @@ Question: {input}
         handle_parsing_errors=True,
     )
 
-    agent_chain.invoke(
-        {
-            "input": initial_input,
-        }
-    )
+    for section_title in section_title_list:
+        input_prompt = f"Can you please review the the manuscript located at: '{path}'?\nPlease review the section titled: '{section_title}'"
+        agent_chain.invoke(
+            {
+                "input": input_prompt,
+            }
+        )
+
+    # agent_chain.invoke(
+    #     {
+    #         "input": initial_input,
+    #     }
+    # )
